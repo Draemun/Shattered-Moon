e5fc5d2bb1c5ae30013d5d6e60348e012b43946c
Draemun <aurora.loprino@gmail.com>
2026-02-20 11:09:43 +0100

    Site update

--- pages/noteworthies.js
+++ pages/noteworthies.js
@@ -19,7 +19,7 @@ const noteworthiesPage = `
         <tr><td>Documentation Commander</td><td>Avira</td></tr>
         <tr><td>Reconnaissance Commanders</td><td>2 Positions Open (they work in tandem)</td></tr>
         <tr><td>Builders Commander</td><td>1 Position Open</td></tr>
-        <tr><td>Providers Commander</td><td>1 Position Open</td></tr>
+        <tr><td>Providers Commander</td><td>Naiko Open</td></tr>
         <tr><td>Void Hunters Commander</td><td>[Staff NPC]</td></tr>
       </table>
     </section>
